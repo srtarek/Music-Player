93fe6421b54d076432eefaf4be945dcce43d7bce
srtarek <49750231+srtarek@users.noreply.github.com>
2020-02-07 12:49:46 +0600

    Update All Button Action
    1. Play Button
    2. Pause Button
    3. Next Button
    4. Previeous Button
    5. Seek Bar
    6. Start Time Duration
    7. All time Duration

--- MP3Player/app/src/main/java/com/srtarek00055/mp3player/Player.java
+++ MP3Player/app/src/main/java/com/srtarek00055/mp3player/Player.java
@@ -10,18 +10,25 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
-public class Player extends AppCompatActivity {
+public class Player extends AppCompatActivity implements View.OnClickListener, MediaPlayer.OnCompletionListener {
+
+//    Ativity id
     ImageView backPlayer,more;
+    Button playBtn,pauseBtn,nxtBtn,prvBtn,allSong;
+    SeekBar seekBar;
+    TextView starTextTime,totalTextTime,songTitle;
 
-    Button playBtn,pauseBtn,nxtBtn;
+    String[] items;
 
-    static MediaPlayer mediaPlayer
-            ;
+    static MediaPlayer mediaPlayer;
     ArrayList<File> mySongs;
     static int position;
     Uri u;
@@ -34,6 +41,8 @@ public class Player extends AppCompatActivity {
     private int backwardTime = 5000;
     private Handler myHandler = new Handler();
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,9 +52,22 @@ public class Player extends AppCompatActivity {
         playBtn = findViewById(R.id.play);
         pauseBtn = findViewById(R.id.pause);
         nxtBtn = findViewById(R.id.next);
+        prvBtn = findViewById(R.id.previous);
+        allSong = findViewById(R.id.allsong);
+//        Song Title
+        songTitle = findViewById(R.id.songTitle);
 
-        btnPlay();
-        nextBtn();
+        starTextTime = findViewById(R.id.startText);
+        totalTextTime = findViewById(R.id.totalText);
+
+        playBtn.setOnClickListener(this);
+        pauseBtn.setOnClickListener(this);
+        nxtBtn.setOnClickListener(this);
+        prvBtn.setOnClickListener(this);
+
+        playBtn.setVisibility(View.GONE);
+        pauseBtn.setVisibility(View.VISIBLE);
+
 //        Pause Button Code Start here
         pauseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,6 +92,32 @@ public class Player extends AppCompatActivity {
             }
         });
 //        Back Button End Code
+//        SeekBar Code Start Here
+        seekBar = findViewById(R.id.seekBar);
+        updateSeekBar = new Thread(){
+            @Override
+            public void run() {
+                int totalDuration = mediaPlayer.getDuration();
+                int currentPosition = 0;
+                seekBar.setMax(totalDuration);
+                while (currentPosition<totalDuration){
+                    try {
+                        sleep(500);
+                        currentPosition = mediaPlayer.getCurrentPosition();
+                        seekBar.setProgress(currentPosition);
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+//                super.run();
+            }
+        };
+//        SeekBar Code End Here
+//        Release MediaPlayer if mediaPlayer Not null
+        if (mediaPlayer !=null){
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
 //        More Button Start Code
         more = findViewById(R.id.more);
         more.setOnClickListener(new View.OnClickListener() {
@@ -86,39 +134,89 @@ public class Player extends AppCompatActivity {
         position = b.getInt("pos");
         u = Uri.parse(mySongs.get(position).toString());
         mediaPlayer = MediaPlayer.create(getApplicationContext(),u);
-//       Start Song First time
-
-        if (mediaPlayer.isPlaying()){
-            mediaPlayer.stop();
-        }else{
         //Start Music Here
-         mediaPlayer.start();
+        mediaPlayer.start();
+        mediaPlayer.setOnCompletionListener(this);
+        if (status){
+            status=false;
+            updateMediaPlayerDuration();
         }
 
     }
-    //        Play Button Code Start here
-    public void nextBtn(){
-        nxtBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-                toast("Next Button");
-            }
-        });
+//        updateMediaPlayerDuration();
     }
-    public void btnPlay(){
-        playBtn.setVisibility(View.GONE);
-        playBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mediaPlayer.start();
-                toast("Play Button");
-                playBtn.setVisibility(View.GONE);
-                pauseBtn.setVisibility(View.VISIBLE);
-            }
-        });
+//    Updatetime Duration from mediaPlayer use SeekBar and Text View code Start Here
+private void updateMediaPlayerDuration(){
+
+    try {
+        if (mediaPlayer !=null) {
+            //Text set Start Time
+            finalTime = mediaPlayer.getDuration();
+            startTime = mediaPlayer.getCurrentPosition();
+            starTextTime.setText(String.format("%d:%d",
+                    TimeUnit.MILLISECONDS.toMinutes((long) startTime),
+                    TimeUnit.MILLISECONDS.toSeconds((long) startTime) -
+                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long)
+                                    startTime)))
+            );
+            //Total Song Time
+            totalTextTime.setText(String.format("%d:%d",
+                    TimeUnit.MILLISECONDS.toMinutes((long) finalTime),
+                    TimeUnit.MILLISECONDS.toSeconds((long) finalTime) -
+                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long)
+                                    finalTime)))
+            );
+            seekBar.setMax(mediaPlayer.getDuration());
+            updateSeekBar.start();
+            myHandler.postDelayed(UpdateSongTime, 100);
+            seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                    mediaPlayer.seekTo(seekBar.getProgress());
+                }
+            });
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
     }
-    //        Play Button Code End here
+}
+//    Updatetime Duration from mediaPlayer use seekBar and Text View code End Here
+//    UpdateSong start time and total time code Start Here
+private Runnable UpdateSongTime = new Runnable() {
+    public void run() {
+        startTime = mediaPlayer.getCurrentPosition();
+        starTextTime.setText(String.format("%d:%d",
+                TimeUnit.MILLISECONDS.toMinutes((long) startTime),
+                TimeUnit.MILLISECONDS.toSeconds((long) startTime) -
+                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.
+                                toMinutes((long) startTime)))
+        );
+        totalTextTime.setText(String.format("%d:%d",
+                TimeUnit.MILLISECONDS.toMinutes((long) finalTime),
+                TimeUnit.MILLISECONDS.toSeconds((long) finalTime) -
+                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long)
+                                finalTime)))
+        );
+
+        myHandler.postDelayed(this, 100);
+    }
+};
+//    UpdateSong start time and total time code End Here
+
 
 //    Text Print Toast
     public void toast(String text){
@@ -128,5 +226,75 @@ public class Player extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+    }
+
+//    All Button Action Code Start Here
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id){
+            case R.id.play:
+                mediaPlayer.start();
+                playBtn.setVisibility(View.GONE);
+                pauseBtn.setVisibility(View.VISIBLE);
+                break;
+            case R.id.pause:
+                mediaPlayer.pause();
+                pauseBtn.setVisibility(View.GONE);
+                playBtn.setVisibility(View.VISIBLE);
+                break;
+//                Next Putton
+            case R.id.next:
+                mediaPlayer.stop();
+                mediaPlayer.release();
+//                position = (position+1)%mySongs.size();
+                if (++position<mySongs.size()) {
+                    u = Uri.parse(mySongs.get(position).toString());
+                    mediaPlayer = MediaPlayer.create(getApplicationContext(), u);
+                    mediaPlayer.setOnCompletionListener(this);
+                    mediaPlayer.start();
+                }
+                break;
+            case R.id.previous:
+                mediaPlayer.stop();
+                mediaPlayer.release();
+//                position = (position-1<0)?mySongs.size()-1:position-1;
+//                if (position-1<0){
+//                    position = mySongs.size()-1;
+//                }else {
+//                    position = position-1;
+//                }
+                if (--position>=0) {
+                    u = Uri.parse(mySongs.get((position)).toString());
+                    mediaPlayer = MediaPlayer.create(getApplicationContext(), u);
+                    mediaPlayer.setOnCompletionListener(this);
+                    mediaPlayer.start();
+                }
+                break;
+
+        }
+//    All Button Action Code End Here
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        if (mediaPlayer!=null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
+
+        if (position>mySongs.size())
+            return;
+        else
+        {
+            u = Uri.parse(mySongs.get(++position).toString());
+            mediaPlayer = MediaPlayer.create(getApplicationContext(),u);
+            mediaPlayer.start();
+            mediaPlayer.setOnCompletionListener(this);
+                 /*myHandler.removeCallbacks(UpdateSongTime);
+                 updateMediaPlayerDuration();*/
+
+        }
+
     }
 }
